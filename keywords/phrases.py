e070765a02fe5c9a60661aa1fa9f575fa24a935a
Steffi Große-Bölting <sgrosseboelting@gmail.com>
2018-07-01 17:25:43 +0200

    Revised comments for functions

--- keywords/phrases.py
+++ keywords/phrases.py
@@ -5,11 +5,9 @@ from rake_nltk import Rake
 
 def get_html(url):
     """
-    Die Funktion erhält als Parameter eine Url und gibt als Ergebnis (mit
-    return) das Html als Text (ein String) zurück.
+    Function gets an url as parameter and returns the html as text (string)
 
-    ACHTUNG: Was passiert, wenn es die Website nicht gibt oder es zu einem
-    anderen Fehler kommt?
+    ToDo: Exception handling for not availble websites and other errors
     """
     page = requests.get(url)
     contents = page.content
@@ -17,6 +15,9 @@ def get_html(url):
     return contents
 
 def tag_visible(element):
+    """
+    Helper function returns false, if element should be excluded, true otherwise.
+    """
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
         return False
     if isinstance(element, Comment):
@@ -25,10 +26,8 @@ def tag_visible(element):
 
 def get_raw_text(html):
     """
-    Die  Funktion erhält als Eingabe mit dem Parameter html einen html-String.
-    Dieser wird dann mit Hilfe dieser Funktion 'bereinigt': Sämtliche tags
-    sollen entfernt werden, damit am Ende nur noch Text übrig bleibt. Der Text
-    wird als String zurückgegeben.
+    Function gets a html-string as parameter. All tags are removed from the
+    html-string. In the end the function returns plain text.
     """
     soup = BeautifulSoup(html, 'html.parser')
     texts = soup.findAll(text = True)
@@ -36,15 +35,12 @@ def get_raw_text(html):
 
     return u" ".join(t.strip() for t in visible_texts)
 
+
 def get_keywords(raw_text, language, phraseMinLength, phraseMaxLength):
     """
-    Diese Funktion erhält als Eingabe einen Text und gibt als Antwort eine
-    Liste von Keywords zurück.
-
-    Dazu verwendet sie die Rake-Bibliothek.
+    Function gets the plain text as input and returns a list of keywords by
+    using the RAKE-library.
     """
-    # Uses stopwords for english from NLTK, and all puntuation characters by
-    # default
     r = Rake(min_length = int(phraseMinLength),
              max_length = int(phraseMaxLength),
              language=language)
@@ -52,25 +48,20 @@ def get_keywords(raw_text, language, phraseMinLength, phraseMaxLength):
     # Extraction given the text.
     r.extract_keywords_from_text(raw_text)
 
-    # To get keyword phrases ranked highest to lowest.
-    # phrases = r.get_ranked_phrases()
-
     # To get keyword phrases ranked highest to lowest with scores.
     return r.get_ranked_phrases_with_scores()
 
 def get_filtered_keywords(keywords):
     """
-    TODO: Keywords filtern. Nicht vergessen: Die List der keywords ist so auf-
-          gebaut [(12.0, "keyword1"), (10.2 "keyword2"), ...]
+    TODO: Filter keywords. Don't forget how list of keywords is build:
+          [(12.0, "keyword1"), (10.2 "keyword2"), ...]
     """
     return keywords
 
 def get_website_keywords(url, language, phraseMinLength, phraseMaxLength):
     """
-    Diese Funktion erhält als Eingabe eine Url und gibt als Antwort eine Reihe
-    von Keywords zurück. Dazu verwendet sie die weiter oben definierten
-    Funktionen. Sie kann dann von anderen Funktionen bspw. in Django aufgerufen
-    werden.
+    Functions gets different parameters and returns keywords consindering the
+    commited options.
     """
     h = get_html(url)
     t = get_raw_text(h)
