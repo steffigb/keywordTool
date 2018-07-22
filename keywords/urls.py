2b7b19ea8ad0f27ff07565bcaecaab8b0e3ea8fd
Steffi Große-Bölting <sgrosseboelting@gmail.com>
2018-07-22 18:25:14 +0200

    login and sign-up

--- keywords/urls.py
+++ keywords/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from . import views
+from accounts import urls
+from django.urls import path, include
+
 
 urlpatterns = [
     path('', views.home, name='index'),
     path('keywords', views.keywords, name='keywords'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('accounts/', include('accounts.urls')),
 ]
