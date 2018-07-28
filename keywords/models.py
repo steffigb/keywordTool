71494c6d5df927f87fa49086e1e98c73050a52db
Steffi Große-Bölting <sgrosseboelting@gmail.com>
2018-07-28 17:14:51 +0200

    add models and migrations. add to admin panel

--- keywords/models.py
+++ keywords/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class KeywordsList(models.Model):
+    description = models.TextField(max_length=200)
+    url = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+class Keyword(models.Model):
+    kw_list = models.ForeignKey(KeywordsList, on_delete=models.CASCADE)
+    phrase = models.CharField(max_length=300)
+    score = models.FloatField(default=0)
