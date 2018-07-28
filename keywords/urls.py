59fc0d898fe8666cf565d575a25f0183dca336bb
Steffi Große-Bölting <sgrosseboelting@gmail.com>
2018-07-28 21:31:27 +0200

    store keyword lists

--- keywords/urls.py
+++ keywords/urls.py
@@ -7,9 +7,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('keywords', views.keywords, name='keywords'),
+    path('keywords', views.keywords_create, name='keywords'),
+    path('<int:list_id>/list/', views.keyword_list, name='list'),
     path('about', views.about, name='about'),
-    path('csv_export', views.csv_export, name='csv_export'),
+    path('<int:list_id>/csv_export/', views.csv_export, name='csv_export'),
     path('accounts/', include('accounts.urls')),
     path('overview', views.keyword_list_overview, name='overview'),
 ]
--- keywords/views.py
+++ keywords/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from . import phrases
 from .models import KeywordsList, Keyword
@@ -11,7 +12,7 @@ def home(request):
     template = loader.get_template('keywords/home.html')
     return HttpResponse(template.render(context, request))
 
-def keywords(request):
+def keywords_create(request):
     request_params = request.POST
     input_url = request_params['url']
     language = request_params['language']
@@ -29,8 +30,24 @@ def keywords(request):
         topNumber = int(numberOfKeywords)
         top_keywords = all_phrases[:topNumber]
 
+    # create a new KeywordsList
+    kw_list = KeywordsList(description="", url=input_url, author=request.user)
+    kw_list.save()
+
+    # add all Keywords to the fresh KeywordsList
+    for keyword in top_keywords:
+        kw_obj = Keyword(kw_list=kw_list, phrase=keyword[1], score=keyword[0])
+        kw_obj.save()
+
+    return HttpResponseRedirect(reverse('list', args=(kw_list.id,)))
+
+def keyword_list(request, list_id):
+    kw_list = get_object_or_404(KeywordsList, pk=list_id)
+    keywords = Keyword.objects.filter(kw_list=kw_list)
+
     context = {
-        "phrases": top_keywords
+        "phrases": keywords,
+        "list": kw_list
     }
     template = loader.get_template('keywords/keywords-output.html')
     return HttpResponse(template.render(context, request))
@@ -40,19 +57,16 @@ def about(request):
     template = loader.get_template('keywords/about.html')
     return HttpResponse(template.render(context, request))
 
-def csv_export(request):
+def csv_export(request, list_id):
+    kw_list = get_object_or_404(KeywordsList, pk=list_id)
+    keywords = Keyword.objects.filter(kw_list=kw_list)
+
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="keywords.csv"'
 
-    csv_data = (
-        (3.0, "shih tzu"),
-        (2.0, "hunde welpe"),
-        (1.0, "lustige hundchen"),
-    )
-
     t = loader.get_template('keywords/csv_export.txt')
     c = {
-        'data': csv_data,
+        'data': keywords,
     }
 
     response.write(t.render(c))
